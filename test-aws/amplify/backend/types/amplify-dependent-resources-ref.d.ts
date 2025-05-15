d33b85b0919c47742d978ad7f3934e9611516e9c
WillyDoo428 <134391576+WillyDoo428@users.noreply.github.com>
2025-05-15 16:59:57 +0200

    Added some functinality

--- test-aws/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ test-aws/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,39 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apid771fbbe": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "testaws41ca56b8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "testawsdd73066f": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamodc85558f": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
